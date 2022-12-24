28d5740b8258bd49c77c62b15b959210f3b20ddd
Luis Henrique Mistura da Silva <luis.silva@atitus.local>
2022-12-23 22:18:15 -0300

    Definĩção e Criação da Estrutura Inicial do Project

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import { useState } from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Menu from "./pages/Menu";
 
 function App() {
+
+  const [login, setLogin] = useState(false)
+
+  const verificarLogin = () => {
+    setLogin(sessionStorage.getItem("login"))
+  }
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login verificarLogin={verificarLogin} />,
+    },
+    {
+      path: "/menu",
+      element: login ? <Menu /> : <Login verificarLogin={verificarLogin} />,
+    },
+  ]);
+
   return (
-    <div className="App">
-      
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
